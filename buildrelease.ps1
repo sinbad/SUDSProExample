42608f8ff394162bac42b64ac22d0d4a6b0cd096
Steve Streeting <steve@stevestreeting.com>
2024-09-30 11:20:37 +0100

    Fix exclusions on build release script

--- buildrelease.ps1
+++ buildrelease.ps1
@@ -14,17 +14,17 @@ $argList = [System.Collections.ArrayList]@()
 $argList.Add("a") > $null
 $argList.Add($zipdst) > $null
 # Standard exclusions
-$argList.Add("-x!$foldername\.git\") > $null
+$argList.Add("-x!$foldername\.git\*") > $null
 $argList.Add("-x!$foldername\.git*") > $null
-$argList.Add("-x!$foldername\.vscode\") > $null
-$argList.Add("-x!$foldername\.buildrelease.ps1") > $null
-$argList.Add("-x!$foldername\Binaries\") > $null
-$argList.Add("-x!$foldername\Intermediate\") > $null
-$argList.Add("-x!$foldername\DerivedDataCache\") > $null
-$argList.Add("-x!$foldername\Saved\") > $null
+$argList.Add("-x!$foldername\.vscode\*") > $null
+$argList.Add("-x!$foldername\buildrelease.ps1") > $null
+$argList.Add("-x!$foldername\Binaries\*") > $null
+$argList.Add("-x!$foldername\Intermediate\*") > $null
+$argList.Add("-x!$foldername\DerivedDataCache\*") > $null
+$argList.Add("-x!$foldername\Saved\*") > $null
 
 # We include the built plugin but don't need the debug symbols
-$argList.Add("-xr!*.pdb") > $null
+$argList.Add("-x!*.pdb") > $null
 
 $argList.Add($zipsrc) > $null
 
